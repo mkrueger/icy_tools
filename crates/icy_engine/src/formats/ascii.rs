15b5e9557d945e0ad4b5e1400b8760a79485e24d
Mike Krüger <mkrueger@posteo.de>
2025-01-28 07:09:06 +0100

    Fixed loading of small text mode formats.
    Minimum height was 24 - that's why lines got cropped, now the file height is correctly taken from input
    instead of doing a load optimization.

--- crates/icy_engine/src/formats/ascii.rs
+++ crates/icy_engine/src/formats/ascii.rs
@@ -52,9 +52,8 @@ impl OutputFormat for Ascii {
     fn load_buffer(&self, file_name: &Path, data: &[u8], load_data_opt: Option<LoadData>) -> EngineResult<crate::Buffer> {
         let load_data = load_data_opt.unwrap_or_default();
         let width = load_data.default_terminal_width.unwrap_or(80);
-        let mut result: Buffer = Buffer::new((width, 25));
-
-        result.is_terminal_buffer = false;
+        let mut result: Buffer = Buffer::new((width, 1));
+        result.is_terminal_buffer = true;
         result.file_name = Some(file_name.into());
         if let Some(sauce) = load_data.sauce_opt {
             result.load_sauce(sauce);
@@ -64,6 +63,7 @@ impl OutputFormat for Ascii {
             result.buffer_type = crate::BufferType::Unicode;
         }
         parse_with_parser(&mut result, &mut parsers::ascii::Parser::default(), &text, true)?;
+        result.is_terminal_buffer = false;
         Ok(result)
     }
 }
--- crates/icy_engine/src/formats/ctrla.rs
+++ crates/icy_engine/src/formats/ctrla.rs
@@ -107,8 +107,8 @@ impl OutputFormat for CtrlA {
     }
 
     fn load_buffer(&self, file_name: &Path, data: &[u8], load_data_opt: Option<LoadData>) -> EngineResult<crate::Buffer> {
-        let mut result = Buffer::new((80, 25));
-        result.is_terminal_buffer = false;
+        let mut result = Buffer::new((80, 1));
+        result.is_terminal_buffer = true;
         result.file_name = Some(file_name.into());
         let load_data = load_data_opt.unwrap_or_default();
         if let Some(sauce) = load_data.sauce_opt {
@@ -120,6 +120,7 @@ impl OutputFormat for CtrlA {
             result.buffer_type = crate::BufferType::Unicode;
         }
         parse_with_parser(&mut result, &mut parsers::ctrla::Parser::default(), &text, true)?;
+        result.is_terminal_buffer = false;
         Ok(result)
     }
 }
