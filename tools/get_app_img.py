e413b26e072fc4cef123979092c5e949d7b2d5fc
Mike Krüger <mkrueger@posteo.de>
2025-02-20 07:57:44 +0100

    Fixed app image file name.

--- tools/get_app_img.py
+++ tools/get_app_img.py
@@ -1,15 +1,15 @@
 import sys
 
-if len(sys.argv) != 3:
-    print("need 1 arguments")
+if len(sys.argv) != 4:
+    print("need 3 arguments")
     sys.exit(1)
 
 match sys.argv[1]:
     case "icy_term":
-        print("Icy_Term_" + sys.argv[2] + ".AppImage")
+        print("Icy_Term_" + sys.argv[2] + "-" + sys.argv[3] + ".AppImage")
     case "icy_draw":
-        print("Icy_Draw_" + sys.argv[2] + ".AppImage")
+        print("Icy_Draw_" + sys.argv[2] + "-" + sys.argv[3] + ".AppImage")
     case "icy_view":
-        print("Icy_View_" + sys.argv[2] + ".AppImage")
+        print("Icy_View_" + sys.argv[2] + "-" + sys.argv[3] + ".AppImage")
     case _:
         print("UNKNOWN APP")
